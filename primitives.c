040478aad292fbc724d577f497ccf050179df31d
vikas148 <vb334531@gmail.com>
2023-02-27 23:56:44 +0530

    first commit

---
+++ primitives.c
@@ -0,0 +1,24 @@
+#include <graphics.h>
+#include <conio.h>
+void main()
+{
+    intgd = DETECT, gm;
+    initgraph(&gd, &gm, "c:\\tc\\bgi");
+    setbkcolor(GREEN);
+    printf("\t\t\t\n\nLINE");
+    line(50, 40, 190, 40);
+    printf("\t\t\n\n\n\nRECTANGLE");
+    rectangle(125, 115, 215, 165);
+    printf("\t\t\t\n\n\n\n\n\n\nARC");
+    arc(120, 200, 180, 0, 30);
+    printf("\t\n\n\n\nCIRCLE");
+    circle(120, 270, 30);
+    printf("\t\n\n\n\nECLIPSE");
+    ellipse(120, 350, 0, 360, 30, 20);
+
+    // triangle
+    line(300, 100, 200, 200);
+    line(300, 100, 400, 200);
+    line(200, 200, 400, 200);
+    getch();
+}
